24cda16ba6802f538660cfeb85f1d7ef568b92de
Johannes Bottesch <johannes.bottesch@innofabrik.de>
2026-01-30 16:22:43 +0100

    adding created_at to flag table

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from datetime import datetime
+from sqlalchemy import Column, DateTime, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
-#Create Base for models
 Base = declarative_base()
 
 class FeatureFlag(Base):
     __tablename__ = "feature_flags"
 
     id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
     name = Column(String, unique=True)
     enabled = Column(Boolean, default=False)
